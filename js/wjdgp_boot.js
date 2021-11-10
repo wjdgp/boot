5187f54fce1750a68b4167ec7aaa9999b9518cf7
wjdgp <qkrwjdgp0213@naver.com>
2021-11-10 15:48:55 +0900

    부트스트랩 3버전으로 제작한 광고웹앱페이지

---
+++ js/wjdgp_boot.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".fi_box_01_e").click(function(){
+        $(".fi_box_01_e").toggleClass("fix");
+    });
+    $(".fi_box_02_qr").click(function(){
+        $(".fi_box_02_qr").toggleClass("fix_02");
+    });
+});
